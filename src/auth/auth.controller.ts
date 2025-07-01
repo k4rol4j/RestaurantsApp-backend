5bc8d9ed3fe9ffe0be8f5e824b83ccdeeea0cdf3
k4rol4j <jedryczka@student.agh.edu.pl>
2025-07-01 08:01:13 +0200

    Poprawki

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('login')
+  login(@Body() body: any) {
+    // Tymczasowo:
+    console.log('Login request received:', body);
+    return { message: 'Login successful (stub)', received: body };
+  }
+}
